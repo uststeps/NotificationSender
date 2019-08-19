5a0a20e4bed3699fd2f2c56d48eb91c507e0d87a
uststeps <uststeps@users.noreply.github.com>
2019-08-19 10:07:08 +0800

    ready for deployment to registrar

--- www/js/send.js
+++ www/js/send.js
@@ -45,10 +45,10 @@ var app = {
 		var toTxt ="";
 
 
-		//toTxtAnd = "andstudent";
-        //toTxtIos =  "iosstudent";
-		toTxtAnd = "gurus-test"; // TEST TOPIC
-        toTxtIos =  "gurus-test"; // TEST TOPIC
+		toTxtAnd = "andstudent";
+        toTxtIos =  "iosstudent";
+		//toTxtAnd = "gurus-test"; // TEST TOPIC
+        //toTxtIos =  "gurus-test"; // TEST TOPIC
 		
         if (titleTxt == "" || titleTxt == null) {
             alert("Title cannot be empty.");
